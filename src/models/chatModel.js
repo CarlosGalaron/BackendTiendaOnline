4094ca97bf18baec2dec10b91ddbac8a5bcf8494
guillermoRF2001 <123741083+guillermoRF2001@users.noreply.github.com>
2025-02-24 13:41:15 +0100

    Update chatModel.js

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -4,39 +4,14 @@ const sequelize = require("../config/db");
 const Chat = sequelize.define(
   "Chat",
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      // Se asume auto-increment para facilitar la inserción de registros
-      autoIncrement: true,
-    },
-    numRoom: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    user1_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    user2_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    mensajes: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    numRoom: { type: DataTypes.STRING, allowNull: false },
+    user1_id: { type: DataTypes.INTEGER, allowNull: false },
+    user2_id: { type: DataTypes.INTEGER, allowNull: false },
+    mensajes: { type: DataTypes.TEXT, allowNull: true },
+    created_at: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
   },
-  {
-    tableName: "chats",
-    timestamps: false, // ya se define el campo created_at
-  }
+  { tableName: "chats", timestamps: false }
 );
 
 module.exports = Chat;
